fe0acf230926e8137e1b75f7caab0ddf3acb384d
Elenatr07 <e.turkina2010@yandex.ru>
2025-05-22 18:36:03 -0400

    adapt 1026

--- scripts/general.js
+++ scripts/general.js
@@ -1,3 +1,24 @@
 const width = window.innerWidth;
 const height = window.innerHeight
 //alert("Ширина экрана: " + width + "px,  Высота экрана" + height);
+
+$(document).ready(function(){
+        $('#side-menu').on('click', function(){
+        if( $('#nav_shadow').hasClass('shadow')) {
+            $('#nav_shadow').removeClass("shadow")
+        } else {
+           $('#nav_shadow').addClass("shadow")  
+        }
+        
+       
+    });
+
+    $('.menu li').on('click', function(){
+        if((width <1026)) {
+         $('body').removeClass('scroll_body');
+         $('#nav_shadow').removeClass("shadow");
+         $('#side-menu').prop('checked', false)
+    } 
+    })
+    
+})
